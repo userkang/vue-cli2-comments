db696185a006a6b9e91a3e0b38a563299f97ee7a
zkk <zhangkangkang@zhangyue.com>
2018-11-01 19:11:44 +0800

    * 添加一些注释

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
 // https://eslint.org/docs/user-guide/configuring
-
+// eslint 的配置文件
 module.exports = {
   root: true,
   parserOptions: {
     parser: 'babel-eslint'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
+  plugins: ['vue'],
+  // 自定义的规则
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,10 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
-
+// 后处理器的配置文件
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    autoprefixer: {}
   }
 }
