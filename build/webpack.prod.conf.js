246a6d378e779713395c0dfeab27429c74ec6156
zkk <zhangkangkang@zhangyue.com>
2018-11-14 15:46:22 +0800

    * 添加一些注释

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -15,9 +15,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : require('../config/prod.env')
+const env =
+  process.env.NODE_ENV === 'testing'
+    ? require('../config/test.env')
+    : require('../config/prod.env')
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -38,10 +39,12 @@ const webpackConfig = merge(baseWebpackConfig, {
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    // 创建一个在编译时可以配置的全局常量
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    // 压缩 JS 代码
+
+    // 压缩混淆 JS 代码
     new UglifyJsPlugin({
       uglifyOptions: {
         compress: {
@@ -51,33 +54,35 @@ const webpackConfig = merge(baseWebpackConfig, {
       sourceMap: config.build.productionSourceMap,
       parallel: true
     }),
+
     // extract css into its own file
-    // 将css提取到单独的文件
+    // 将 css 提取到单独的文件
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css'),
       // Setting the following option to `false` will not extract CSS from codesplit chunks.
       // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
       // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-      allChunks: true,
+      allChunks: true
     }),
+
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
-    // 优化、最小化 css 代码，如果只简单使用 extract-text-plugin 可能会造成css重复
+    // 优化、最小化 css 代码，如果只简单使用 extract-text-plugin 可能会造成 css 重复
     // 具体原因可以看关于 optimize-css-assets-webpack-plugin 的介绍
     new OptimizeCSSPlugin({
       cssProcessorOptions: config.build.productionSourceMap
         ? { safe: true, map: { inline: false } }
         : { safe: true }
     }),
+
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     // 将产品文件的引用注入到 index.html
     new HtmlWebpackPlugin({
-      filename: process.env.NODE_ENV === 'testing'
-        ? 'index.html'
-        : config.build.index,
+      filename:
+        process.env.NODE_ENV === 'testing' ? 'index.html' : config.build.index,
       template: 'index.html',
       inject: true,
       minify: {
@@ -94,12 +99,20 @@ const webpackConfig = merge(baseWebpackConfig, {
       // 注入依赖的时候按照依赖先后顺序进行注入，比如，需要先注入vendor.js，再注入app.js
       chunksSortMode: 'dependency'
     }),
+
     // keep module.id stable when vendor modules does not change
+    // 该插件会根据模块的相对路径生成一个四位数的hash作为模块id, 建议用于生产环境。
     new webpack.HashedModuleIdsPlugin(),
+
     // enable scope hoisting
+    // 过去 webpack 打包时的一个取舍是将 bundle 中各个模块单独打包成闭包。
+    // 这些打包函数使你的 JavaScript 在浏览器中处理的更慢。
+    // 相比之下，一些工具像 Closure Compiler 和 RollupJS 可以提升(hoist)或者预编译所有模块到一个闭包中，
+    // 提升你的代码在浏览器中的执行速度。这个插件就是进行“作用域提升的”
     new webpack.optimize.ModuleConcatenationPlugin(),
+
     // split vendor js into its own file
-    // 将所有从 node_modules 中引入的js提取到vendor.js，即抽取库文件
+    // 将所有从 node_modules 中引入的js提取到 vendor.js，即抽取库文件
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       minChunks(module) {
@@ -107,12 +120,11 @@ const webpackConfig = merge(baseWebpackConfig, {
         return (
           module.resource &&
           /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
+          module.resource.indexOf(path.join(__dirname, '../node_modules')) === 0
         )
       }
     }),
+
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     // 从 vendor 中提取出 manifest
@@ -120,9 +132,11 @@ const webpackConfig = merge(baseWebpackConfig, {
       name: 'manifest',
       minChunks: Infinity
     }),
+
     // This instance extracts shared chunks from code splitted chunks and bundles them
     // in a separate chunk, similar to the vendor chunk
     // see: https://webpack.js.org/plugins/commons-chunk-plugin/#extra-async-commons-chunk
+    // 从代码中提取共享内容，将它们打包到一个独立的块中。和 vendor.js 类似。
     new webpack.optimize.CommonsChunkPlugin({
       name: 'app',
       async: 'vendor-async',
@@ -131,7 +145,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
 
     // copy custom static assets
-    // 将static文件夹里面的静态资源复制到 dist/static
+    // 将 static 文件夹里面的静态资源复制到 dist/static
     new CopyWebpackPlugin([
       {
         from: path.resolve(__dirname, '../static'),
@@ -151,9 +165,7 @@ if (config.build.productionGzip) {
       asset: '[path].gz[query]',
       algorithm: 'gzip',
       test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
+        '\\.(' + config.build.productionGzipExtensions.join('|') + ')$'
       ),
       threshold: 10240,
       minRatio: 0.8
@@ -163,7 +175,8 @@ if (config.build.productionGzip) {
 
 // 如果启动了report，则通过插件给出webpack构建打包后的产品文件分析报告
 if (config.build.bundleAnalyzerReport) {
-  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin
   webpackConfig.plugins.push(new BundleAnalyzerPlugin())
 }
 
--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,14 @@
 <!-- 你可以在这里写一些页面的js的逻辑代码 -->
 <script>
 export default {
-  name: "App"
-};
+  name: 'App'
+}
 </script>
 
 <!-- 页面需要的CSS样式 -->
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
